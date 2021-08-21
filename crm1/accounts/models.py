03972fb3046f120b8abc3edcbdd1a0bd933b62d8
keerthinc <74804111+keerthinc@users.noreply.github.com>
2021-08-21 21:02:43 +0530

    Database Relationships

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -8,6 +8,12 @@ class Customer(models.Model):
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
+	def __str__(self):
+		return self.name
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
 		return self.name
@@ -21,8 +27,12 @@ class Product(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True)
+	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
+	tags = models.ManyToManyField(Tag)
+
+	def __str__(self):
+		return self.name
 
 class Order(models.Model):
 	STATUS = (
@@ -31,7 +41,11 @@ class Order(models.Model):
 			('Delivered', 'Delivered'),
 			)
 
-	#customer = 
-	#product = 
+	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+	note = models.CharField(max_length=1000, null=True)
+
+	def __str__(self):
+		return self.product.name
